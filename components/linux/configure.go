e2598dd604540755406e25e91275e80c51f87974
hbc <me@hbc.rocks>
2026-02-17 10:38:38 -0800

    feat: install required packages

--- components/linux/configure.go
+++ components/linux/configure.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"os"
+	"os/exec"
 	"strings"
 
 	"google.golang.org/protobuf/types/known/anypb"
@@ -33,7 +34,11 @@ func (a *configureBaseOSAction) ApplyAction(
 		return nil, err
 	}
 
-	if err := a.ensureSysctlConfig(); err != nil {
+	if err := a.ensurePackages(ctx); err != nil {
+		return nil, err
+	}
+
+	if err := a.ensureSysctlConfig(ctx); err != nil {
 		return nil, err
 	}
 
@@ -45,6 +50,50 @@ func (a *configureBaseOSAction) ApplyAction(
 	}
 
 	return actions.ApplyActionResponse_builder{Item: item}.Build(), nil
+}
+
+var requiredPackages = []string{
+	"iptables",
+	"jq",
+}
+
+func (a *configureBaseOSAction) hasPackage(pkg string) bool {
+	_, err := exec.LookPath(pkg)
+	if err == nil {
+		// package is already installed
+		return true
+	}
+
+	return false
+}
+
+func (a *configureBaseOSAction) ensurePackages(ctx context.Context) error {
+	// TODO: we should use distro-specific methods to detect and install packages.
+	packagesToInstall := map[string]struct{}{}
+	for _, pkg := range requiredPackages {
+		if !a.hasPackage(pkg) {
+			packagesToInstall[pkg] = struct{}{}
+		}
+	}
+	if len(packagesToInstall) == 0 {
+		return nil
+	}
+
+	cmd := utilexec.New()
+
+	if err := cmd.CommandContext(ctx, "apt", "update").Run(); err != nil {
+		return err
+	}
+
+	args := []string{"install", "-y"}
+	for k := range packagesToInstall {
+		args = append(args, k)
+	}
+	if err := cmd.CommandContext(ctx, "apt", args...).Run(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // TODO: this should be merged with the input request
@@ -60,7 +109,7 @@ kernel.panic_on_oops = 1
 
 const sysctlConfigPath = "/etc/sysctl.d/999-sysctl-aks.conf"
 
-func (a *configureBaseOSAction) ensureSysctlConfig() error {
+func (a *configureBaseOSAction) ensureSysctlConfig(ctx context.Context) error {
 	expectedConfig := []byte(strings.TrimSpace(sysctlSettings))
 
 	currentConfig, err := os.ReadFile(sysctlConfigPath)
@@ -79,7 +128,7 @@ func (a *configureBaseOSAction) ensureSysctlConfig() error {
 	if err := utilio.WriteFile(sysctlConfigPath, expectedConfig, 0644); err != nil {
 		return err
 	}
-	if err := utilexec.New().Command("sysctl", "--system").Run(); err != nil {
+	if err := utilexec.New().CommandContext(ctx, "sysctl", "--system").Run(); err != nil {
 		return err
 	}
 
