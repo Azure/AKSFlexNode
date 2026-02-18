82f1fb5db10c4899ec9267d3f60ef491ba50e432
Anson Qian <ansonqian@microsoft.com>
2026-02-18 20:18:19 +0000

    refactoring

--- pkg/components/kubelet/kubelet_installer.go
+++ pkg/components/kubelet/kubelet_installer.go
@@ -213,7 +213,7 @@ func (i *Installer) createKubeletDefaultsFile() error {
 	// Add --rotate-certificates flag only for bootstrap token authentication
 	rotateCertsFlag := ""
 	if i.config.IsBootstrapTokenConfigured() {
-		rotateCertsFlag = "  --rotate-certificates=true \\\n"
+		rotateCertsFlag = "  --rotate-certificates=true\n"
 	}
 
 	kubeletDefaults := fmt.Sprintf(`KUBELET_NODE_LABELS="%s"
@@ -242,8 +242,8 @@ KUBELET_FLAGS="\
   --read-only-port=0  \
   --resolv-conf=/run/systemd/resolve/resolv.conf  \
   --streaming-connection-idle-timeout=4h  \
-%s  --tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256 \
-  "`,
+  --tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256 \
+%s  "`,
 		strings.Join(labels, ","),
 		i.config.Node.Kubelet.Verbosity,
 		apiserverClientCAPath,
