8f2b7ff74fa137b077cf2f8ed14299dbd927d354
Anson Qian <ansonqian@microsoft.com>
2026-02-18 22:29:38 +0000

    fix typo

--- pkg/components/kubelet/kubelet_installer.go
+++ pkg/components/kubelet/kubelet_installer.go
@@ -241,8 +241,9 @@ KUBELET_FLAGS="\
   --read-only-port=0  \
   --resolv-conf=/run/systemd/resolve/resolv.conf  \
   --streaming-connection-idle-timeout=4h  \
+  --rotate-certificates=%t \
   --tls-cipher-suites=TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_256_GCM_SHA384,TLS_RSA_WITH_AES_128_GCM_SHA256 \
-  --rotate-certificates=%t"`,
+  "`,
 		strings.Join(labels, ","),
 		i.config.Node.Kubelet.Verbosity,
 		apiserverClientCAPath,
